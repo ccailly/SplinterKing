f10de6e1fadfb0360ea5d6df7d3a306a0d2b5cf1
Fragment <clemcailly@gmail.com>
2023-02-07 17:22:09 +0100

    💄 Adding Flame Regular font (font-flame)

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
         extend: {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                flame: ['Flame Regular', ...defaultTheme.fontFamily.sans],
             },
         },
     },
